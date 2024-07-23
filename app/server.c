203bebca318665d5540d055cf1435733a4057642
inkfin <inkfinite@qq.com>
2024-07-24 00:03:15 +0800

    6th stage: Concurrent connections

--- app/server.c
+++ app/server.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +11,7 @@
 /*** defines ***/
 
 #define BUFFER_SIZE 1500
+#define MAX_PTHREAD_NUM 8
 
 #define REQ_USER_AGENT "GET /user-agent "
 #define REQ_ECHO "GET /echo/"
@@ -29,6 +31,98 @@ char const *const fmt_reply_200 =
     "\r\n"
     /* Response body */
     "%s";
+
+/*** threads ***/
+
+/* pthread func that handle one client request */
+void *handle_connection(void *pclient_fd)
+{
+    int client_fd = *(int *)pclient_fd;
+    while (1)
+    {
+        char sz_recv_buf[BUFFER_SIZE];
+        char sz_send_buf[BUFFER_SIZE];
+        char const *sz_send_message;
+
+        const int recv_numbytes = recv(client_fd, sz_recv_buf, BUFFER_SIZE - 1, 0);
+        if (recv_numbytes == -1)
+        {
+            printf("Recv failed: %s \n", strerror(errno));
+            return NULL;
+        }
+        else if (recv_numbytes == 0)
+        {
+            printf("Connection closed\n");
+            break;
+        }
+        else
+        {
+            printf("Received message success:\n"
+                   "<length=%d>\n"
+                   "/***content-beg***/\n"
+                   "%s<end>\n"
+                   "/***content-end***/\n",
+                   recv_numbytes, sz_recv_buf);
+        }
+
+        if (strncmp(sz_recv_buf, REQ_USER_AGENT, /* prefix length */ strlen(REQ_USER_AGENT)) == 0)
+        {
+            char *pc_start, *pc_end;
+            pc_start = strstr(sz_recv_buf + strlen(REQ_USER_AGENT), "User-Agent: ");
+            pc_end = strstr(pc_start, "\r\n");
+            if (pc_start != NULL && pc_end != NULL)
+            {
+                char sz_temp_buf[BUFFER_SIZE];
+
+                pc_start += strlen("User-Agent: ");
+                memcpy(sz_temp_buf, pc_start, (pc_end - pc_start));
+                sz_temp_buf[pc_end - pc_start] = '\0';
+                sprintf(sz_send_buf, fmt_reply_200, (int)strlen(sz_temp_buf), sz_temp_buf);
+                sz_send_message = sz_send_buf;
+            }
+            else
+            {
+                printf("parse error: can't find header User-Agent\n");
+                return NULL;
+            }
+        }
+        else if (strncmp(sz_recv_buf, REQ_ECHO, strlen(REQ_ECHO)) == 0)
+        {
+            // first char after request str
+            char const *const sz_echo_str = strtok(sz_recv_buf + strlen(REQ_ECHO), " ");
+            if (sz_echo_str)
+            {
+                sprintf(sz_send_buf, fmt_reply_200, (int)strlen(sz_echo_str), sz_echo_str);
+                sz_send_message = sz_send_buf;
+            }
+        }
+        else if (strncmp(sz_recv_buf, REQ_ROOT, strlen(REQ_ROOT)) == 0)
+        {
+            sz_send_message = reply_200;
+        }
+        else
+        {
+            sz_send_message = reply_404;
+        }
+
+        const int sent_numbytes = send(client_fd, sz_send_message, strlen(sz_send_message), 0);
+        if (sent_numbytes == -1)
+        {
+            printf("Send failed: %s \n", strerror(errno));
+            return NULL;
+        }
+        else
+        {
+            printf("Send message success:\n"
+                   "/***content-beg***/\n"
+                   "%s<end>\n"
+                   "/***content-end***/\n",
+                   sz_send_message);
+        }
+    }
+
+    return NULL;
+}
 
 /*** exec ***/
 
@@ -80,91 +174,35 @@ int main()
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
 
-    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) == -1)
-    {
-        printf("Create client failed: %s \n", strerror(errno));
-        return 1;
-    }
-    printf("Client connected\n");
+    // create pthread
+    pthread_t arr_pthread[MAX_PTHREAD_NUM];
+    int arr_pthread_ret[MAX_PTHREAD_NUM];
+    int pthread_idx = 0;
 
-    int recv_numbytes, sent_numbytes;
-    char pc_recv_buf[BUFFER_SIZE];
-    char pc_send_buf[BUFFER_SIZE];
-    char pc_temp_buf[BUFFER_SIZE];
-    char *pc_echo_str;
-    char const *pc_reply_str;
     while (1)
     {
-        recv_numbytes = recv(client_fd, pc_recv_buf, BUFFER_SIZE - 1, 0);
-        if (recv_numbytes == -1)
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) == -1)
         {
-            printf("Recv failed: %s \n", strerror(errno));
-            return 1;
-        }
-        else if (recv_numbytes == 0)
-        {
-            printf("Connection closed\n");
+            printf("Create client failed: %s \n", strerror(errno));
             break;
         }
         else
         {
-            printf("Received message success:\n"
-                   "<length=%d>\n"
-                   "/***content-beg***/\n"
-                   "%s<end>\n"
-                   "/***content-end***/\n",
-                   recv_numbytes, pc_recv_buf);
+            arr_pthread_ret[pthread_idx] =
+                pthread_create(&arr_pthread[pthread_idx], NULL, handle_connection, (void *)&client_fd);
+            ++pthread_idx;
+            printf("Client connected\n");
         }
+    }
 
-        if (strncmp(pc_recv_buf, REQ_USER_AGENT, /* prefix length */ strlen(REQ_USER_AGENT)) == 0)
-        {
-            char *pc_start, *pc_end;
-            pc_start = strstr(pc_recv_buf + strlen(REQ_USER_AGENT), "User-Agent: ");
-            pc_end = strstr(pc_start, "\r\n");
-            if (pc_start != NULL && pc_end != NULL)
-            {
-                pc_start += strlen("User-Agent: ");
-                memcpy(pc_temp_buf, pc_start, (pc_end - pc_start));
-                pc_temp_buf[pc_end - pc_start] = '\0';
-                sprintf(pc_send_buf, fmt_reply_200, (int)strlen(pc_temp_buf), pc_temp_buf);
-                pc_reply_str = pc_send_buf;
-            }
-            else
-            {
-                printf("parse error: can't find header User-Agent\n");
-                return 1;
-            }
-        }
-        else if (strncmp(pc_recv_buf, REQ_ECHO, strlen(REQ_ECHO)) == 0)
-        {
-            if ((pc_echo_str = strtok(/* first char after request str */ pc_recv_buf + strlen(REQ_ECHO), " ")))
-            {
-                sprintf(pc_send_buf, fmt_reply_200, (int)strlen(pc_echo_str), pc_echo_str);
-                pc_reply_str = pc_send_buf;
-            }
-        }
-        else if (strncmp(pc_recv_buf, REQ_ROOT, strlen(REQ_ROOT)) == 0)
-        {
-            pc_reply_str = reply_200;
-        }
-        else
-        {
-            pc_reply_str = reply_404;
-        }
+    for (int i = 0; i < MAX_PTHREAD_NUM; ++i)
+    {
+        pthread_join(arr_pthread[i], NULL);
+    }
 
-        if ((sent_numbytes = send(client_fd, pc_reply_str, strlen(pc_reply_str), 0)) == -1)
-        {
-            printf("Send failed: %s \n", strerror(errno));
-            return 1;
-        }
-        else
-        {
-            printf("Send message success:\n"
-                   "/***content-beg***/\n"
-                   "%s<end>\n"
-                   "/***content-end***/\n",
-                   pc_reply_str);
-        }
+    for (int i = 0; i < MAX_PTHREAD_NUM; ++i)
+    {
+        printf("pthread[%d] returns %d", i, arr_pthread_ret[i]);
     }
 
     close(server_fd);
